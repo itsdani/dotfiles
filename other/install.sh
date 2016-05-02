89e8051c4120fd72f741d2f1bec01d4131a43306
Robert Doczi <robert.doczi@incquerylabs.com>
2016-05-03 00:50:58 +0200

    added noconfirm to yaourt installs

--- other/install.sh
+++ other/install.sh
@@ -3,6 +3,6 @@
 sudo pacman --noconfirm -S wget 
 
 
-yaourt -S the_platinum_searcher
-yaourt -S gtk-theme-arc
-yaourt -S numix-icon-theme-git
+yaourt --noconfirm -S the_platinum_searcher
+yaourt --noconfirm -S gtk-theme-arc
+yaourt --noconfirm -S numix-icon-theme-git
