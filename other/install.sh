8cd41b4d95bf25cb8858916854a0f125a87301cb
Robert Doczi <doczi.r@gmail.com>
2016-05-03 20:21:31 +0200

    reenabled other installs

--- other/install.sh
+++ other/install.sh
@@ -3,6 +3,6 @@
 sudo pacman --noconfirm -S wget 
 
 
-#yaourt --noconfirm -S the_platinum_searcher
-#yaourt --noconfirm -S gtk-theme-arc
-#yaourt --noconfirm -S numix-icon-theme-git
+yaourt --noconfirm -S the_platinum_searcher
+yaourt --noconfirm -S gtk-theme-arc
+yaourt --noconfirm -S numix-icon-theme-git
