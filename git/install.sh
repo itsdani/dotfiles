9d042a6e4edf359ddc76f98249d2b017fc011b01
Robert Doczi <robert.doczi@incquerylabs.com>
2016-05-03 02:17:23 +0200

    additional bugfixes

--- git/install.sh
+++ git/install.sh
@@ -1,6 +1,6 @@
 
-name="$(cat name.cry | gpg)"
-email="$(cat email.cry | gpg)"
+name="$(cat ./git/name.cry | gpg)"
+email="$(cat ./git/email.cry | gpg)"
 
 git config --global user.name "$name"
 git config --global user.email "$email"
